a4e88d2e0e4df5d69ddb4114a01af6ff674cb867
Ali Joshan <joshansanjar@gmail.com>
2026-02-20 23:24:23 +0430

    Reset scroll position on route change

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Footer from './components/Footer'
 import Hero from './components/Hero'
 import Navbar from './components/NavBar'
 import ProjectsSection from './components/ProjectsSection'
+import ScrollToTop from './components/ScrollToTop'
 import SkillsSection from './components/SkillsSection'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -15,6 +16,7 @@ function App() {
     <BrowserRouter>
       <Navbar />
 
+      <ScrollToTop />
       <Routes>
         <Route
           path="/"
